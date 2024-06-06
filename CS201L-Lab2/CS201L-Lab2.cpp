d31ccb92d029072a93a95fe50e9cf047c1faf567
Joan Gladbach <jjgr29@umsystem.edu>
2024-06-06 15:27:42 -0500

    Add project files.

---
+++ CS201L-Lab2/CS201L-Lab2.cpp
@@ -0,0 +1,28 @@
+// NAME:
+// LAB SECTION:
+// LECTURE SECTION:
+// 
+// CS201L-Lab2.cpp : This program will play the game rock paper scissors between
+//                   two people by:
+//                   a. accepting 2 names and the number of rounds to play from the user
+//                   b. verifying that the number of rounds is numeric & between 0-10
+//                   c. if the number is not valid, the program will generate a
+//                      random number between 1-10 (inclusive) and provide
+//                      a message that the user entered an invalid number and this is 
+//                      the number of of games to be played 
+//                   d. the program will randomly generating a number 0(Rock), 1(Paper), 
+//                      2(Scissors) for each player
+//                   e. the program will identify the winner of the round and track the
+//                      score for each player
+//                   f. when the rounds have been played, the program will provide a
+//                      message about which player won
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Welcome to Rock, Paper, Scissors!\n";
+    cout << "Enter 2 player names & the number of rounds, separated by spaces: ";
+
+}
+
